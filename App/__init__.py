614a5fe2946a6dddc51b13131274531065206b78
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-07-16 20:13:09 +0300

    JWT configuration

--- App/__init__.py
+++ App/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from App.extensions import db, bcrypt
+from flask_jwt_extended import JWTManager
+
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
@@ -7,6 +10,7 @@ def create_app():
 
     db.init_app(app)
     bcrypt.init_app(app)
+    jwt.init_app(app) 
 
     with app.app_context():
         from App import models  
