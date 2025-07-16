1295b64a0c3faf57ce98e6f767186e6cc9a0003e
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-07-16 20:13:46 +0300

    JWT configuration

--- App/Config.py
+++ App/Config.py
@@ -4,4 +4,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:kihikah@localhost:5432/farmart_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your_jwt_secret_key'
