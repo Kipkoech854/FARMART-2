8cc51f53d18331b480f464c8102946e0a402134e
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-07-16 16:24:37 +0300

    Add configuration class for database and secret key management

--- App/Config.py
+++ App/Config.py
@@ -1,1 +1,7 @@
-configuration for database location
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:kihikah@localhost:5432/farmart_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+
