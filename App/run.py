8599786845e6173185a8950c0f5e6d6371980d95
chiscobyte <chiscoenock@gmail.com>
2025-07-17 13:28:23 +0300

    Update run.py

--- App/run.py
+++ App/run.py
@@ -6,7 +6,7 @@ app = create_app("development")
 if __name__ == '__main__':
 
     app.run(debug=True, host="0.0.0.0", port=5555)
-=======
+
     app.run(debug = True, host = "0.0.0.0", port = 5555)
 
 
