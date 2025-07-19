b6b94980ded2b79e5f2603a6d2d5934a7f14fa35
shuaib-hassan <shuaib.hassan@student.moringaschool.com>
2025-07-19 22:29:15 +0300

    add search and sort functionality to animal endpoint

--- App/models/Feedback.py
+++ App/models/Feedback.py
@@ -4,7 +4,11 @@ class Feedback(db.Model):
     __tablename__ = 'feedback'
     id = db.Column(db.String, primary_key=True)
     user_id = db.Column(db.String, db.ForeignKey('users.id'), nullable=False)
-    farmer_id = db.Column(db.String, db.ForeignKey('farmers.id'), nullable=False)
+    farmer_id = db.Column(db.String, db.ForeignKey('users.id'), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     comment = db.Column(db.String, nullable=True)
     image_url = db.Column(db.String(255))
+    
+    # Relationships
+    user = db.relationship('User', foreign_keys=[user_id], backref='feedback_given')
+    farmer = db.relationship('User', foreign_keys=[farmer_id], backref='feedback_received')
--- App/models/Users.py
+++ App/models/Users.py
@@ -11,7 +11,7 @@ class User(db.Model):
 
     __tablename__ = 'users'
 
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(120), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
